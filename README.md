fdaebf420ac9a047849130ab7708a15c4cdf60d3
Shuyang Shi <shuyang790@gmail.com>
2016-04-06 20:06:12 +0800

    add: 1-CMU-CSD (scrapy)

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ Collectors for information from CS MS./Ph.D. programs of several universities.
 A tool for course Practice of Computer Network.
 
 
-- [ ] 1 CMU
+- [x] 1 CMU (CSD only)
 - [x] 1 MIT
 - [x] 1 SU
 - [x] 1 UCB
 - [x] 5 UIUC
 - [x] 6 CU
 - [x] 6 UW
-- [x] 8 PU
+- [x] 8 PU '
 - [x] 9 GIT
-- [x] 9 UTA '
+- [x] 9 UTA
--- src/1-CMU/faculty/faculty/spiders/faculty_spider.py
+++ src/1-CMU/faculty/faculty/spiders/faculty_spider.py
@@ -1,33 +1,83 @@
 import scrapy
 import os
 from os import system
+from faculty.items import FacultyItem
 
 class FacultySpider(scrapy.Spider):
     name = "faculty"
-    allowed_domains = ["stanford.edu"]
+    allowed_domains = ["cmu.edu"]
     start_urls = [
-            "http://www-cs.stanford.edu/directory/faculty"
+            "https://www.csd.cs.cmu.edu/directory/faculty"
     ]
 
-    def person_parse(self, response):
-        name = response.xpath('//title/text()').extract()[0].replace(' ', '_').replace('|', '')\
-                .lower().replace("'s", "").replace("home_page", "").replace("_page", "")\
-                .replace('\n', '').strip('_').strip()
-        filename = "../../../data/1-SU/" + name + "/index.html"
+    def person_info_parse(self, response):
+        url = response.url
+        item = response.meta['item']
+        name = item['name']
+        filename = url[len(item['link']):].rstrip('/')
+        if filename == "":
+            return
+        target_filename = "../../../data/1-CMU/" + name.encode('utf-8') + "/" + filename.lstrip('/').encode('utf-8')
+        system('mkdir -p "' + '/'.join(target_filename.split('/')[:-1]) + '"')
+        if os.path.isdir(target_filename):
+            target_filename = target_filename + "/index.html"
+        if target_filename.split("/")[-1].find('.') == -1:
+            target_filename = target_filename + ".html"
+        with open(target_filename, "w") as f:
+            f.write(response.body)
+
+    def person_index_parse(self, response):
+        item = response.meta['item']
+        name = item['name']
+        if name == "":
+            return
+        filename = "../../../data/1-CMU/" + name.encode('utf-8') + "/index.html"
         print response.url,
         if os.path.isfile(filename):
             print " ... (skip)"
             return
         else:
             print " ..."
-        system('mkdir "../../../data/1-SU/' + name + '"')
+        system('mkdir "../../../data/1-CMU/' + name.encode('utf-8') + '"')
         with open(filename, "w") as f:
             f.write(response.body)
+        for sel in response.xpath('//a/@href'):
+            url = sel.extract()
+            if url.find(":") != -1 and url.startswith(item['link']):
+                url= url[len(item['link']):]
+            if url.find(":") == -1 and not (url[-4:] in [".pdf", ".avi", ".mp4", ".mov", ".jpg", ".png", ".ppt"])\
+                    and not (url[-3:] in [".ps"]):
+                url = response.urljoin(url)
+#                yield scrapy.Request(url, meta={'item':item}, callback=self.person_info_parse)
+
 
     def parse(self, response):
-        for sel in response.xpath("//table/tr/td/a"):
-            name = sel.xpath("text()").extract()[0].replace(' ', '_')
-            link = sel.xpath("@href").extract()[0]
-            if (name.upper() != "CLICK_HERE"):
+        # even
+        for sel in response.xpath('//table/tbody/*[contains(concat(" ", normalize-space(@class), " "), " even ")]'):
+            name = " ".join(sel.xpath('td/a/text()').extract()[:2])
+            link = sel.xpath('td/a/@href').extract()[0]
+            if name.strip() == "":
+                continue
+            if link.find(":") == -1:
+                link = response.urljoin(link)
+            if (name.upper() != "CLICK HERE") and link.startswith("http"):
                 print "%s @@@ '%s'" % (name, link)
-                yield scrapy.Request(link, callback=self.person_parse)
+                item = FacultyItem()
+                item['name'] = name
+                item['link'] = link.rstrip('/')
+                yield scrapy.Request(url=link, meta={'item': item}, callback=self.person_index_parse)
+
+        # odd
+        for sel in response.xpath('//table/tbody/*[contains(concat(" ", normalize-space(@class), " "), " odd ")]'):
+            name = " ".join(sel.xpath('td/a/text()').extract()[:2])
+            link = sel.xpath('td/a/@href').extract()[0]
+            if name.strip() == "":
+                continue
+            if link.find(":") == -1:
+                link = response.urljoin(link)
+            if (name.upper() != "CLICK HERE") and link.startswith("http"):
+                print "%s @@@ '%s'" % (name, link)
+                item = FacultyItem()
+                item['name'] = name
+                item['link'] = link.rstrip('/')
+                yield scrapy.Request(url=link, meta={'item': item}, callback=self.person_index_parse)
